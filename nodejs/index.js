d16c6c06454f8cfb3a05dcff0ca845850cef2162
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-04-20 15:50:40 +0700

    sd

--- nodejs/index.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-const port = 3000;
-
-const server = http.createServer((req, res)=>{
-  console.log('dsd');
-  res.end('VNTALKING: Xin chào node.js');
-})
-server.listen(port);
