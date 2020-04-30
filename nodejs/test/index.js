7418f56099fd7b294b897f6c63d4b3e9dfb2961a
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-04-30 11:06:15 +0700

    sd

--- nodejs/test/index.js
+++
@@ -1,8 +0,0 @@
-const http = require("http");
-http.createServer((req, res)=>{
-  res.writeHead(200, {
-    'Conten-Type' : 'text/plain'
-  });
-  res.end('Hello World');
-}).listen(3000);
-console.log('Server running at http://127.0.0.1:3000/');
