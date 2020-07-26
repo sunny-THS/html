752393996d8fb14ffdb75adb1fd760aa7d93da74
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-07-27 01:04:30 +0700

    sd

--- @/Arduino/qd/app.js
+++ @/Arduino/qd/app.js
@@ -8,35 +8,36 @@ function GetData() {
   var mode = document.getElementById('mode_');
   firebase.database().ref( /*NO AUTH*/ ).once('value').then(snapshot => {
     //console.log(snapshot.val());
-    document.getElementById('nhietdo').innerText = Math.round(snapshot.val().ThongSo.NhietDo*100)/100;
+    document.getElementById('nhietdo').innerText = Math.round(snapshot.val().ThongSo.NhietDo * 100) / 100;
     document.getElementById('doam').innerText = snapshot.val().ThongSo.DoAm;
 
-    if (snapshot.val().PumpIsWork==1) {
+    if (snapshot.val().PumpIsWork == 1) {
       pump.innerText = "On";
-      pump.style.background  = "rgb(0, 255, 252)";
+      pump.style.background = "rgb(0, 255, 252)";
       pump.style.color = "#fff";
-    }else
-    {
+    } else {
       pump.innerText = "Off";
-      pump.style.background  = "red";
+      pump.style.background = "red";
       pump.style.color = "#000";
     }
 
-    if (snapshot.val().Mode==1) {
-      mode.style.background  = "green";
-    }else
-    {
-      mode.style.background  = "red";
+    if (snapshot.val().Mode == 1) {
+      mode.style.background = "green";
+    } else {
+      mode.style.background = "red";
     }
 
-  }).catch(err => {console.error(err);});
+  }).catch(err => {
+    console.error(err);
+  });
+
   setTimeout(GetData, 10);
 }
 
 function SetData(pump, t, h, mode) {
   firebase.database().ref( /*NO AUTH*/ ).set({
     PumpIsWork: pump,
-    ThongSo:{
+    ThongSo: {
       NhietDo: t,
       DoAm: h
     },
@@ -46,32 +47,39 @@ function SetData(pump, t, h, mode) {
 
 function pump() {
   firebase.database().ref( /*NO AUTH*/ ).once('value').then(snapshot => {
-    let pump_ = snapshot.val().PumpIsWork==1 ? false : true;;
-    if (snapshot.val().Mode==1) {
+    let pump_ = snapshot.val().PumpIsWork == 1 ? false : true;;
+    if (snapshot.val().Mode == 1) {
       if (pump_) {
         document.getElementById('pump_').innerText = "On";
         SetData(1, snapshot.val().ThongSo.NhietDo, snapshot.val().ThongSo.DoAm, 1);
-      }else
-      {
+      } else {
         document.getElementById('pump_').innerText = "Off";
         SetData(0, snapshot.val().ThongSo.NhietDo, snapshot.val().ThongSo.DoAm, 1);
       }
     }
-  }).catch(err => {console.error(err);});
+  }).catch(err => {
+    console.error(err);
+  });
 }
 
 function mode() {
   firebase.database().ref( /*NO AUTH*/ ).once('value').then(snapshot => {
-    let mode_ = snapshot.val().Mode==1 ? false : true;
+    let mode_ = snapshot.val().Mode == 1 ? false : true;
     if (mode_) {
       SetData(0, snapshot.val().ThongSo.NhietDo, snapshot.val().ThongSo.DoAm, 1);
-    }else
-    {
+    } else {
       SetData(0, snapshot.val().ThongSo.NhietDo, snapshot.val().ThongSo.DoAm, 0);
     }
-  }).catch(err => {console.error(err);});
+  }).catch(err => {
+    console.error(err);
+  });
 }
 
-function outpage() {
-  SetData(0, snapshot.val().ThongSo.NhietDo, snapshot.val().ThongSo.DoAm, 0);
-}
+window.addEventListener("beforeunload", function(e) {
+  var confirmationMessage = "\o/";
+
+  (e || window.event).returnValue = confirmationMessage; // Gecko + IE
+  return confirmationMessage;
+  /* Safari, Chrome, and other
+   * WebKit-derived browsers */
+});
