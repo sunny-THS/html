d972d2f84fd979f7b2527e9b792b6542e1017faa
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-09-04 06:53:49 +0700

    sd

--- @/Arduino/qd/app.js
+++ @/Arduino/qd/app.js
@@ -1,95 +1,77 @@
-window.addEventListener("beforeunload", function(e) {
-  var confirmationMessage = "\o/";
-  SetData(0, 0, 0, 0);
+const API_URL = 'https://misting-acb22.firebaseio.com/ThongSo.json?&auh=M2IpMvlhcYjDRCCXaXrrdmlgw2POdaH9iC5fZpJa';
 
-  (e || window.event).returnValue = confirmationMessage; // Gecko + IE
-  return confirmationMessage;
-  /* Safari, Chrome, and other
-   * WebKit-derived browsers */
-});
-document.addEventListener("DOMContentLoaded", event => {
-  var app = firebase.app();
-  GetData();
-});
+const temperature = document.querySelector('#_t');
+const humidity = document.querySelector('#_h');
+const pump = document.querySelector('.pump');
 
-function GetData() {
-  var pump = document.getElementById('pump_');
-  var mode = document.getElementById('mode_');
-  firebase.database().ref( /*NO AUTH*/ ).once('value').then(snapshot => {
-    // console.log(snapshot.val().SaveData);
+var _t, _h, _p, ip;
 
+const database = firebase.database();
 
-    document.getElementById('nhietdo').innerText = Math.round(snapshot.val().ThongSo.NhietDo * 100) / 100;
-    document.getElementById('doam').innerText = snapshot.val().ThongSo.DoAm;
+setTimeout(getIPAddress, 500);
+GetDB();
 
-    if (snapshot.val().PumpIsWork == 1) {
-      pump.innerText = "On";
-      pump.style.background = "rgb(0, 255, 252)";
-      pump.style.color = "#fff";
-    } else {
-      pump.innerText = "Off";
-      pump.style.background = "red";
-      pump.style.color = "#000";
+function GetDB() {
+  fetch(API_URL)
+  .then(res => res.json())
+  .then(db => {
+    _t = db.NhietDo;
+    temperature.textContent = `${db.NhietDo} ᵒC`;
+
+    _h = db.DoAm;
+    humidity.textContent = `${db.DoAm} %`;
+
+    _p = db.PumpIsWork;
+    if(db.PumpIsWork == 1) {
+      pump.style.background = 'green';
+    }else {
+      pump.style.background = 'red';
     }
-
-    if (snapshot.val().Mode == 1) {
-      mode.style.background = "green";
-    } else {
-      mode.style.background = "red";
-    }
-
-  }).catch(err => {
-    console.error(err);
   });
-
-  setTimeout(GetData, 10);
+  setTimeout(GetDB, 500);
 }
 
-function SetData(pump, t, h, mode) {
-  firebase.database().ref( /*NO AUTH*/ ).set({
-    PumpIsWork: pump,
-    ThongSo: {
-      NhietDo: t,
-      DoAm: h
-    },
-    Mode: mode
-  });
+function getIPAddress() {
+  fetch(API_URL)
+    .then(res => res.json())
+    .then(db => {
+      let scores = db.IP;
+      ip = db.IP;
+      let devices = Object.keys(scores);
+      devices.forEach(device => {
+        const div = document.querySelector('._IP');
 
-  // firebase.database().ref('/SaveData').push({
-  //   DataTime: Date().substr(0, 24),
-  //   NhietDo: t,
-  //   DoAm: h,
-  //   PumpIsWork: pump,
-  //   Mode: mode
-  // });
+        const name = document.createElement('h2');
+        name.textContent = `${scores[device].name}: `;
+
+        const ip = document.createElement('a');
+        ip.href = `http://${scores[device].val}/`;
+        ip.textContent = `${scores[device].val}`;
+        ip.target = '_blank';
+
+        const br = document.createElement('br')
+
+        div.appendChild(name);
+        div.appendChild(ip);
+        div.appendChild(br);
+      });
+    });
 }
 
-function pump() {
-  firebase.database().ref( /*NO AUTH*/ ).once('value').then(snapshot => {
-    let pump_ = snapshot.val().PumpIsWork == 1 ? false : true;;
-    if (snapshot.val().Mode == 1) {
-      if (pump_) {
-        document.getElementById('pump_').innerText = "On";
-        SetData(1, snapshot.val().ThongSo.NhietDo, snapshot.val().ThongSo.DoAm, 1);
-      } else {
-        document.getElementById('pump_').innerText = "Off";
-        SetData(0, snapshot.val().ThongSo.NhietDo, snapshot.val().ThongSo.DoAm, 1);
-      }
-    }
-  }).catch(err => {
-    console.error(err);
-  });
-}
-
-function mode() {
-  firebase.database().ref( /*NO AUTH*/ ).once('value').then(snapshot => {
-    let mode_ = snapshot.val().Mode == 1 ? false : true;
-    if (mode_) {
-      SetData(0, snapshot.val().ThongSo.NhietDo, snapshot.val().ThongSo.DoAm, 1);
-    } else {
-      SetData(0, snapshot.val().ThongSo.NhietDo, snapshot.val().ThongSo.DoAm, 0);
-    }
-  }).catch(err => {
-    console.error(err);
-  });
-}
+// function SetData() {
+//   database.ref().set({
+//     DoAm: _h,
+//     NhietDo: _t,
+//     IP: {
+//       DHT: {
+//         name: ip['DHT'].name,
+//         val: ip['DHT'].val
+//       },
+//       Relay: {
+//         name: ip['Relay'].name,
+//         val: ip['Relay'].val
+//       }
+//     },
+//     PumpIsWork: _p,
+//   });
+// }
