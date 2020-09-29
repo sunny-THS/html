3fcfc64cf1f3af18e9c909fee263eff1fa96a9da
Sunny <42699455+sunny-THS@users.noreply.github.com>
2020-09-29 11:59:19 +0700

    sd

--- @/Arduino/qd/app.js
+++ @/Arduino/qd/app.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://misting-acb22.firebaseio.com/ThongSo.json?&auh=M2IpMvlhcYjDRCCXaXrrdmlgw2POdaH9iC5fZpJa';
+const API_URL = 'https://fir-9d177.firebaseio.com/ThongSo.json?&auh=YA2HldfmtXM9ZtNtAz5yetMTgxFpd8Gu2Dx8seta';
 
 const temperature = document.querySelector('#_t');
 const humidity = document.querySelector('#_h');
@@ -16,7 +16,7 @@ function GetDB() {
   .then(res => res.json())
   .then(db => {
     _t = db.NhietDo;
-    temperature.textContent = `${db.NhietDo} ᵒC`;
+    temperature.textContent = `${Math.round(db.NhietDo*100)/100} ᵒC`;
 
     _h = db.DoAm;
     humidity.textContent = `${db.DoAm} %`;
